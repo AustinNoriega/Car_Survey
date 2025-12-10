c40383ea3ff0277955ac2588d3ccb33e5a3ede0f
austi <austinnoriega12@gmail.com>
2025-12-09 18:48:24 -0800

    Updated

--- main.py
+++ main.py
@@ -25,14 +25,15 @@ age_ranges = ['<18',
 with st.form(key='Car_survey'):
     city = st.text_input("Enter City")
     state = st.text_input("Enter State")
-    age = st.multiselect("Enter Age Range", options=age_ranges)
+    age = st.selectbox("Enter Age Range", options=age_ranges)
+    st.text("Please input the number of how important these aspects in a sports car are below")
     speed = st.slider("Straight Line Speed",min_value=0, max_value=10, value=1)
     handling = st.slider("Handling/Suspension",min_value=0, max_value=10, value=1)
     visuals = st.slider("Visual Aestetics",min_value=0, max_value=10, value=1)
     feel = st.slider("Driving Response/Feeling",min_value=0, max_value=10, value=1)
     project_ideas = st.text_input("What is your current/next project car")
     new_car = st.text_area("Would you buy a new sports car from the lot?")
-    cost = st.slider("How much money would you spend on a new sports car (2020-Present)", 0,500000,step=1000)
+    cost = st.slider("How much money would you spend on a new sports car (2020-Present)", 0,250000,step=1000)
     submit = st.form_submit_button("Submit")
     if submit:
         if not city or not state or not cost:
@@ -40,12 +41,6 @@ with st.form(key='Car_survey'):
         else:
             st.write("Thanks for submitting!")
             with con.session as s:
-                s.execute(text("CREATE TABLE IF NOT EXISTS cars (ID INTEGER,City TEXT,State TEXT, Age FLOAT, Speed INTEGER, Handling Integer, Visual INTEGER, Feeling INTEGER, Project_idea TEXT, "
-                               "New_car TEXT, Cost_for_new INTEGER,PRIMARY KEY(ID AUTOINCREMENT));"))
-
-                s.execute(text("INSERT INTO cars (City, State, Age, Speed, Handling, Visual, Feeling, Project_idea, New_car,Cost_for_new) VALUES (:city, :state, :age, :speed,"
-                               " :handling, :visual, :feeling, :project_idea, :new_car, :cost_for_new);"),
+                s.execute(text("INSERT INTO cars (City, State, Age, Speed, Handling, Visual, Feeling, Project_idea, New_car,Cost_for_new) VALUES (:city, :state, :age, :speed, :handling, :visual, :feeling, :project_idea, :new_car, :cost_for_new);"),
                     params = dict(city = city, state = state, age = age, speed = speed, handling = handling, visual = visuals, feeling = feel, project_idea = project_ideas, cost_for_new = cost,  new_car = new_car)),
                 s.commit()
-            df = con.query("select * from cars", ttl = 0 )
-            st.dataframe(df)
